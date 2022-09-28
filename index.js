225b973a0c5803cd60b8b3788dc48f5174637794
Davide Poveromo <davide.poveromo@enel.com>
2022-09-28 16:00:11 +0200

    + https support + canonicalise function in C14nCanonicalization

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var ExclusiveCanonicalisation = require("./lib/algorithm/exclusive-canonicalisation");
-var C14nCanonicalization = require("./lib/algorithm/c14n-canonicalisation");
+var C14nCanonicalization = require("./lib/algorithm/c14n-canonicalisation").C14nCanonicalization;
+var C14nCanonicalizationWithComments = require("./lib/algorithm/c14n-canonicalisation").C14nCanonicalizationWithComments;
 
 var builtIn = {
   algorithms: {
     "http://www.w3.org/2001/10/xml-exc-c14n#": function(options) {
+      return new ExclusiveCanonicalisation(options);
+    },
+    "https://www.w3.org/2001/10/xml-exc-c14n#": function(options) {
       return new ExclusiveCanonicalisation(options);
     },
     "http://www.w3.org/2001/10/xml-exc-c14n#WithComments": function(options) {
@@ -11,10 +15,21 @@ var builtIn = {
       options.includeComments = true;
       return new ExclusiveCanonicalisation(options);
     },
+    "https://www.w3.org/2001/10/xml-exc-c14n#WithComments": function(options) {
+      options = Object.create(options || null);
+      options.includeComments = true;
+      return new ExclusiveCanonicalisation(options);
+    },
     "http://www.w3.org/TR/2001/REC-xml-c14n-20010315": function() {
       return new C14nCanonicalization();
     },
+    "https://www.w3.org/TR/2001/REC-xml-c14n-20010315": function() {
+      return new C14nCanonicalization();
+    },
     "http://www.w3.org/TR/2001/REC-xml-c14n-20010315#WithComments": function() {
+      return new C14nCanonicalizationWithComments();
+    },
+    "https://www.w3.org/TR/2001/REC-xml-c14n-20010315#WithComments": function() {
       return new C14nCanonicalizationWithComments();
     }
   },
